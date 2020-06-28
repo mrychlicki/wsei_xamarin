5a7eb6002212391d130c5378a51b11ad7aff9dab
mrychlicki <mateusz.rychlicki@microsoft.wsei.edu.pl>
2020-06-28 10:01:41 +0200

    lab2

---
+++ AirMonitor/AirMonitor/Convert/VTP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AirMonitor.Convert
+{
+    public class VTP : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!double.TryParse(value?.ToString(), out var result)) return value;
+
+            return result * 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+using System.Windows.Input;
+using AirMonitor.Views;
+using Xamarin.Forms;
+
+namespace AirMonitor.ViewModels
+{
+    public class HomeViewModel : ContentView
+    {
+        private readonly INavigation _navigation;
+
+        public HomeViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+        }
+
+        private ICommand _goToDetailsCommand;
+        public ICommand GoToDetailsCommand => _goToDetailsCommand ?? (_goToDetailsCommand = new Command(OnGoToDetails));
+
+        private void OnGoToDetails()
+        {
+            _navigation.PushAsync(new DetailsPage());
+        }
+    }
+}
